c46e5adadb0e5035c90aa7f87e114ad4c1d4f799
anthony vu <anthony_vu@live.ca>
2018-01-25 14:50:43 -0800

    Completed Basic CI to Good Website conversion

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -19,6 +19,22 @@ class Welcome extends Application
 	 */
 	public function index()
 	{
+		$pix = $this->images->newest();
+		
+		foreach ($pix as $picture)
+			$cells[] = $this->parser->parse('_cell', (array) $picture, true);
+			
+		$this->load->library('table');
+		$parms = array(
+			'table_open' => '<table class="gallery">',
+			'cell_start' => '<td class="oneimage">',
+			'cell_alt_start' => '<td class="oneimage">'
+		);
+		$this->table->set_template($parms);
+		
+		$rows = $this->table->make_columns($cells, 3);
+		$this->data['thetable'] = $this->table->generate($rows);
+		
 		$this->data['pagebody'] = 'welcome';
 		$this->render();
 	}
--- changelog.md
+++ changelog.md
@@ -46,3 +46,36 @@ Release Date: Jan 25, 2018
     - about.php
     - welcome.php
     - gallery.php
+
+## *Version 0.3*
+
+Release Date: Jan 25, 2018
+
+## New Components
+
+-   None
+
+## Updated Components
+-   controllers
+    -   Welcome.php
+    -   About.php
+    -   Galley.php
+    -   .gitignore
+    -   autoload.php
+    -   database.php
+    -   Gallery.php
+    -   Welcome.php
+    -   gallery.php
+    -   welcome.php
+
+-   core
+    -   MY_Controller.php
+	
+-   models
+    -   Images.php
+   
+-   views
+    - about.php
+    - welcome.php
+    - gallery.php
+    - _cell.php
